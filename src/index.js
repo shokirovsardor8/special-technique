4efcd9bbefbb0046ca6b2bf6c6884d7451674631
Sardor <69262569+shokirovsardor8@users.noreply.github.com>
2022-06-04 09:52:29 +0500

    last

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Provider } from 'react-redux';
 import Store from './Store';
 import { BrowserRouter } from 'react-router-dom';
